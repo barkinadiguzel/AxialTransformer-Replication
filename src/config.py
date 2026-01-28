3d1849374f572df666de9e3901c27be5e4855283
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-01-29 01:53:33 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,16 @@
+class AxialConfig:
+    # image shape
+    H = 32
+    W = 32
+
+    # embedding
+    vocab_size = 256
+    dim = 256
+    ff_hidden_dim = 512
+
+    # attention
+    num_heads = 8
+
+    # depth
+    num_upper_layers = 4   
+    num_row_layers = 4     
